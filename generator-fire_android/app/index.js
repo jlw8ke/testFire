7b59b66344794e20dcb126ae00f09757bacf0fe9
Justin Washington <jwashington@mobiquityinc.com>
2014-09-25 12:26:45 -0400

    arguments from prompt sent to script

--- generator-fire_android/app/index.js
+++ generator-fire_android/app/index.js
@@ -1,39 +1,84 @@
-var yeoman = require('yeoman-generator')
-var self
+var yeoman = require('yeoman-generator'),
+spawn = require('child_process').spawn,
+path = require("path"),
+self
 
 module.exports = yeoman.generators.Base.extend({
-  promptTask: function () {
-  	self = this
-    var done = this.async()
-    this.log("Initializing Android project")
+	promptTask: function () {
+		self = this
+		var done = this.async()
+		this.log("Initializing Android Gradle project")
 
-    this.prompt([{
-      type : 'input',
-      name : 'name',
-      message : 'Project name: ',
-      required : true
-    }, {
-    	type : 'input',
-    	name : 'path',
-    	message : 'Project path: ',
-    	required : true
-    },  {
-    	type : 'input',
-    	name : 'package',
-    	message : 'Project package: ',
-    	required : true
-    }, {
-    	type : 'input',
-    	name : 'target',
-    	message : 'Build target: ',
-    	required : true
-    }], function (answers) {
-      createAndroidProject(answers)
-      done()
-    }.bind(this))
-  }
+		this.prompt([{
+			type : 'input',
+			name : 'name',
+			message : 'Project name: ',
+			validate : function(input) {
+				var done = this.async();
+				setTimeout(function() {
+					if (!input) {
+						done("A project name is required");
+						return;
+					}
+					done(true);
+				}, 10);
+			}
+		}, {
+			type : 'input',
+			name : 'path',
+			message : 'Project path: ',
+			validate : function(input) {
+				var done = this.async();
+				setTimeout(function() {
+					if (!input) {
+						done("A project path is required");
+						return;
+					}
+					done(true);
+				}, 10);
+			}		
+		},  {
+			type : 'input',
+			name : 'package',
+			message : 'Project package: ',
+			validate : function(input) {
+				var done = this.async();
+				setTimeout(function() {
+					if (!input) {
+						done("A project package is required");
+						return;
+					}
+					done(true);
+				}, 10);
+			}		
+		}, {
+			type : 'input',
+			name : 'target',
+			message : 'Build target: ',
+			validate : function(input) {
+				var done = this.async();
+				setTimeout(function() {
+					if (!input) {
+						done("A project target is required");
+						return;
+					}
+					done(true);
+				}, 10);
+			}		
+		}], function (answers) {
+			createAndroidProject(answers)
+			done()
+		}.bind(this))
+	}
 })
 
 function createAndroidProject(params) {
 	self.log(params)
+	var file_loc = path.join(__dirname, "init_android.sh")
+	self.log("Script location : " + file_loc)
+	var init_android = spawn(file_loc, 
+		[params.name, params.path, params.package, params.target])	
+	init_android.stdout.on('data', function (data) {
+  		console.log('stdout: ' + data);
+	})
 }
---
+++ generator-fire_android/app/init_android.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+name=$1
+path=$2
+package=$3
+target=$4
+
+echo "Init started: $name $path $package $target"
