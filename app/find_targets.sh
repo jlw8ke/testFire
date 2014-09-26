1a15003390250c01f81c024027a0c4b12b71ddba
Justin Washington <jwashington@mobiquityinc.com>
2014-09-26 14:02:18 -0400

    android targets found from grep to populate android sdk choices

---
+++ app/find_targets.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+android list target | grep -e 'id:.*android\|Name: Android.*'
--- app/index.js
+++ app/index.js
@@ -1,48 +1,42 @@
 var yeoman = require('yeoman-generator'),
 spawn = require('child_process').spawn,
 path = require("path"),
-self
+self,
+targets,
+projectName
 
 module.exports = yeoman.generators.Base.extend({
+	generateTargetsTask: function() {
+		self = this
+		findAvailableTargets()
+	},
 	promptTask: function () {
 		self = this
 		var done = this.async()
-		var currentName
 		this.log("Initializing Android Gradle project")
-
 		this.prompt([{
 			type : 'input',
 			name : 'name',
-			message : 'Project name: ',
+			message : 'Project name:',
 			validate : function(input) {
 				var done = this.async();
-				currentName = input
 				setTimeout(function() {
 					if (!input) {
-						done("A project name is required");
-						return;
+						done("A project name is required")
+						return
 					}
-					done(true);
-				}, 10);
+					done(true)
+				}, 10)
 			}
 		}, {
 			type : 'input',
 			name : 'path',
-			message : 'Project path: ',
-			validate : function(input) {
-				var done = this.async();
-				setTimeout(function() {
-					if (!input) {
-						done("A project path is required");
-						return;
-					}
-					done(true);
-				}, 10);
-			}		
+			message : 'Project path:',
+			default : './'
 		},  {
 			type : 'input',
 			name : 'package',
-			message : 'Project package: ',
+			message : 'Project package:',
 			validate : function(input) {
 				var done = this.async();
 				setTimeout(function() {
@@ -56,84 +50,18 @@ module.exports = yeoman.generators.Base.extend({
 		}, {
 			type : 'list',
 			name : 'target',
-			message : 'Build target: ',
-			choices: [
-				{
-					name: "Android 4.0",
-					value: "android-14"
-				},
-				{
-					name: "Android 4.0.3",
-					value: "android-15"
-				},
-				{
-					name: "Android 4.1.2",
-					value: "android-16"
-				},
-				{
-					name: "Android 4.2.2",
-					value: "android-17"
-				},
-				{
-					name: "Android 4.3",
-					value: "android-18"
-				},
-				{
-					name: "Android 4.4.2",
-					value: "android-19"
-				},
-				{
-					name: "Android 4.4W",
-					value: "android-20"
-				},
-				{
-					name: "Android L (preview)",
-					value: "android-L"
-				}
-			]		
+			message : 'Build target:',
+			choices: targets
 		}, {
 			type : 'list',
 			name : 'min_target',
-			message : 'Minimum sdkt: ',
-			choices: [
-				{
-					name: "Android 4.0",
-					value: "android-14"
-				},
-				{
-					name: "Android 4.0.3",
-					value: "android-15"
-				},
-				{
-					name: "Android 4.1.2",
-					value: "android-16"
-				},
-				{
-					name: "Android 4.2.2",
-					value: "android-17"
-				},
-				{
-					name: "Android 4.3",
-					value: "android-18"
-				},
-				{
-					name: "Android 4.4.2",
-					value: "android-19"
-				},
-				{
-					name: "Android 4.4W",
-					value: "android-20"
-				},
-				{
-					name: "Android L (preview)",
-					value: "android-L"
-				}
-			]		
+			message : 'Minimum sdk:',
+			choices: targets	
 		}, 
 		{
 			type : 'input',
 			name : 'activity',
-			message : 'Main Activity: ',
+			message : 'Main Activity:',
 			default : "MainActivity"
 		}], function (answers) {
 			createAndroidProject(answers)
@@ -150,4 +78,22 @@ function createAndroidProject(params) {
 	init_android.stdout.on('data', function (data) {
   		console.log('' + data);
 	})
+}
+
+function findAvailableTargets() {
+	self.log("Finding available targets")
+	targets = new Array()
+	var file_loc = path.join(__dirname, "find_targets.sh")
+	var find_targets = spawn(file_loc);
+	find_targets.stdout.on('data', function (data) {
+  		var lines = String(data).split('\n')
+  		for(var i = 0;i < lines.length-1;i+=2) {
+  			var value = lines[i].split('or')
+  			var name = lines[i+1].trim().substring(5)
+  			var target = new Object()
+  			target.name=name.trim()
+  			target.value=value[1].trim()
+  			targets.push(target)
+  		}  		
+	});
 }
